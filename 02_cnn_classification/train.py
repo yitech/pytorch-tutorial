39a177adc2a75d6cd45bd535767cd7e86f44d552
yitech <coastq22889@icloud.com>
2020-12-31 09:43:06 +0800

    Add confusion metric

--- 02_cnn_classification/train.py
+++ 02_cnn_classification/train.py
@@ -3,6 +3,8 @@ import os
 import argparse
 import configparser
 
+import numpy as np
+from sklearn.metrics import confusion_matrix
 import torch
 from torchvision import transforms
 from torch.utils.data import DataLoader, random_split
@@ -34,7 +36,7 @@ for section in config.sections():
 
 # Load data
 batch_size = 32
-transform = transforms.Compose([transforms.Resize((224, 224)),
+transform = transforms.Compose([transforms.Resize((64, 64)),
                                transforms.ToTensor(),
                                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
                                ])
@@ -43,6 +45,7 @@ map_dataset = MapDataset(csv_file=os.path.join(ABS_DIR, config["Train"]["LabelSo
                          , img_dir=os.path.join(ABS_DIR, config["Train"]["ImageSource"])
                          , trans=transform)
 
+n_class = map_dataset.num_class
 n_valid = int(len(map_dataset) * 0.12)
 train_set, val_set = random_split(map_dataset, [len(map_dataset) - n_valid, n_valid])
 print("training set: {}, validation set: {}".format(len(train_set), len(val_set)))
@@ -51,7 +54,7 @@ data_loader = {"train": DataLoader(train_set, shuffle=True, batch_size=batch_siz
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("device on {}".format(device))
-net = Net(map_dataset.num_class)
+net = Net(n_class)
 net = net.to(device)
 print(net)
 # img = torch.randn(64 * 64 * 3 * 2).reshape(2, 3, 64, 64)
@@ -76,28 +79,31 @@ for t in range(n_epoch):
             net.eval()
         # keep track of training and validation loss
         running_loss = 0.0
-        running_correct = 0.0
+        running_confusion = np.zeros(shape=(n_class, n_class), dtype=np.float)
 
         for data, target in data_loader[phase]:
             data, target = data.to(device), target.to(device)
-            print(target.shape)
+            # print(target.shape)
             with torch.set_grad_enabled(phase == 'train'):
                 # feed the input
                 preds = net(data)
-                print(preds.shape)
+                # print(preds.shape)
                 # calculate the loss
                 loss = loss_func(preds, target)
-                # preds[preds == torch.max(preds, axis=1)] = 1.0
-                # preds[preds < 1.0] = 0.0
-                # preds = preds.to(torch.float32)
+                running_confusion += confusion_matrix(
+                    target.numpy(),
+                    torch.argmax(preds, dim=1).numpy(),
+                    labels=np.arange(n_class)
+                )
                 if phase == 'train':
                     optimizer.zero_grad()
                     loss.backward()
                     optimizer.step()
             running_loss += loss.item() * data.size(0)
+            # print("item: {}. size: {}".format(loss.item(), data.size(0)))
 
         epoch_loss = running_loss / len(data_loader[phase].dataset)
-        epoch_acc = running_correct / len(data_loader[phase].dataset)
+        epoch_acc = np.trace(running_confusion) / len(data_loader[phase].dataset)
         print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
 
 
